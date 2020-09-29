f0e1272bcce80ca05d1356cf3d8c8c944ef8ca7d
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2020-09-29 17:57:23 +0530

    Self Learning of 09/29

--- R/Introduction_To_R_Seminar.R
+++ R/Introduction_To_R_Seminar.R
@@ -33,3 +33,25 @@ log(100,10)
 log(8,2)
 
 # https://stats.idre.ucla.edu/stat/data/intro_r/intro_r_interactive.html
+#Vectors
+#     Vectors, the fundamental data structure in R, are one-dimensional and homogeneous.
+
+# Creating vectors
+# create a vector
+first_vec <- c(1, 3, 5)
+first_vec
+## [1] 1 3 5
+
+
+
+# length() returns the number of elements
+
+char_vec <- c("these", "are", "some", "words")
+length(char_vec)
+length(first_vec)
+## [1] 4
+
+
+# the result of this comparison is a logical vector
+first_vec > c(2, 2, 2)
+## [1] FALSE  TRUE  TRUE
