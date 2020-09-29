f19f43fdf5717d3ca11f082d58327b6cc89a0499
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2020-09-29 18:12:06 +0530

    Logit Regression in R 09/29

--- LOGIT REGRESSION R DATA ANALYSIS EXAMPLES/logit_regression.R
+++ LOGIT REGRESSION R DATA ANALYSIS EXAMPLES/logit_regression.R
@@ -1,4 +1,6 @@
 #setting up path 
+#     imp  link : https://cran.r-project.org/web/packages/HSAUR/
+#               : https://stats.idre.ucla.edu/r/dae/logit-regression/
 
 setwd("E:\\Self_GitKraken\\Working_Repo_GitHub\\UCLA-learn\\LOGIT REGRESSION R DATA ANALYSIS EXAMPLES")
 getwd()
@@ -28,3 +30,40 @@ summary(mydata)
 sapply(mydata, sd)
 
 ?sapply
+
+## two-way contingency table of categorical outcome and predictors we want
+## to make sure there are not 0 cells
+xtabs(~admit + rank, data = mydata)
+
+
+#     The code below estimates a logistic regression model using the glm (generalized linear model) function. 
+#     First, we convert rank to a factor to indicate that rank should be treated as a categorical variable.
+mydata$rank <- factor(mydata$rank)
+summary(mydata)
+summary(mydata$rank)
+
+
+#   logit test
+mylogit <- glm(admit ~ gre + gpa + rank, data = mydata, family = "binomial")
+
+summary(mylogit)
+
+
+## CIs using profiled log-likelihood
+
+confint(mylogit)
+
+## CIs using standard errors
+confint.default(mylogit)
+
+
+wald.test(b = coef(mylogit), Sigma = vcov(mylogit), Terms = 4:6)
+
+
+
+
+l <- cbind(0, 0, 0, 1, -1, 0)
+wald.test(b = coef(mylogit), Sigma = vcov(mylogit), L = l)
+
+
+
